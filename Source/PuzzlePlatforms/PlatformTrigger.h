6c58d7a871c17e65a981165fec6f2cc6a3935fe4
Lucas Harris <lucasharris.dev@outlook.com>
2023-04-26 15:40:11 -0400

    Detect when player enters or exits pressure plate mesh

--- Source/PuzzlePlatforms/PlatformTrigger.h
+++ Source/PuzzlePlatforms/PlatformTrigger.h
@@ -24,7 +24,12 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 private:
-	UPROPERTY(VisibleAnywhere) // allows us to edit the details in blueprints of pointers
+	UPROPERTY(VisibleAnywhere) // allows me to edit the details in blueprints of pointers
 	class UBoxComponent* TriggerVolume;
 
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 };
